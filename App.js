c234b7ac55467e616b28c1e1128796e02e35ab7f
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-09-08 22:24:20 +0530

    fix: app init before fonts loading

--- App.js
+++ App.js
@@ -1,54 +1,46 @@
+import { ActivityIndicator } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
-import { Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, DefaultTheme,StackNavigator } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { useEffect } from 'react';
-import * as Font from 'expo-font';
+const Stack = createStackNavigator();
 
-const Stack=createStackNavigator();
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
-import Home from './screens/Home'
-import Details from './screens/Details';
-
-const theme={
+const theme = {
   ...DefaultTheme,
-  colors:{
+  colors: {
     ...DefaultTheme.colors,
-    background:"transparent"
-  }
-}
-const customFonts={
-  'InterBold': require("./assets/fonts/Inter-Bold.ttf"),
-  'InterRegular': require("./assets/fonts/Inter-Regular.ttf"),
-}
-const App=()=> {
-  const [loaded] = useFonts({
-    'InterBold': require("./assets/fonts/Inter-Bold.ttf"),
-    'InterSemiBold': require("./assets/fonts/Inter-SemiBold.ttf"),
-    'InterMedium': require("./assets/fonts/Inter-Medium.ttf"),
-    'InterRegular': require("./assets/fonts/Inter-Regular.ttf"),
-    'InterLight': require("./assets/fonts/Inter-Light.ttf"),
-  });
-  useEffect(()=>{
-    (async ()=>{
+    background: "transparent",
+  },
+};
 
-      await Font.loadAsync(customFonts)
-    })();
-  },[])
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName="Home"
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
+      {fontsLoaded ? (
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="Home"
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      ) : (
+        <ActivityIndicator />
+      )}
     </NavigationContainer>
   );
-
-}
-export default App
+};
+export default App;
